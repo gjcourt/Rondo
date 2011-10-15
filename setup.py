8261520564f90ea966994f51f88c09a40cb2c70f
George Courtsunis <gjcourt@gmail.com>
2011-10-15 13:26:57 -0700

    add setup script to rondo

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Rondo',
+      version='0.1',
+      description='Command line music player',
+      author='George J Courtsunis',
+      author_email='gjcourt@gmail.com',
+      packages=[],
+     )
+

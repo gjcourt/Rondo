1db979e1da2ee94b8810618e85c49f9eb1970a5b
George Courtsunis <gjcourt@gmail.com>
2011-10-16 13:52:08 -0700

    add a gitignore file, finish fleshing out the requirements install

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+
+install_requires = None
+with open('requirements.txt', 'r') as reqs:
+    install_requires = [line for line in reqs if not line.strip(' \n').startswith('#')]
 
 setup(name='Rondo',
       version='0.1',
       description='Command line music player',
       author='George J Courtsunis',
       author_email='gjcourt@gmail.com',
-      packages=[],
+      install_requires = install_requires,
      )
 
